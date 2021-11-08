ff1e4f73bc018ed9c6fb3c97ed0084548c269d78
Mateo Gregory <mateo.gregory.kommit@gmail.com>
2021-11-08 10:19:17 -0500

    added users models and authentication using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-
+  devise_for :users
+  root 'home#index'
+  resources :home, only: [:index]
   resources :articles do
     resources :comments
   end
+
 end
